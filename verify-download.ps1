f3cb6bfb07b7570b7b195057f733e1bda4fac1d2
dtfrench <60610232+dtfrench@users.noreply.github.com>
2021-02-04 15:31:18 +0530

    Update verify-download.ps1

--- verify-download.ps1
+++ verify-download.ps1
@@ -13,7 +13,7 @@ $program_hash = Get-FileHash -Path $program_to_check -Algorithm $preferred_hash
 Write-Host("Your program hash is:", $program_hash.Hash)
 
 #Takes the hash from user input that the calculated hash from the program is going to be checked against. 
-$hash_to_check_against = Read-Host -Prompt "`nEnter the hash that you want the program hash to be compared against."
+$hash_to_check_against = Read-Host -Prompt "`nEnter the hash that you want the program hash to be compared against"
 
 #The hashes are compared, the user is informed if they match, and the hashes are printed out. 
 if ($program_hash.Hash -eq $hash_to_check_against) {
